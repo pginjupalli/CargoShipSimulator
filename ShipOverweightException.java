d9699288e3ff48940efa9deaef478f3d81859ea1
Pooja Ginjupalli <113191715+pginjupalli@users.noreply.github.com>
2023-09-28 15:58:45 -0400

    Created ShipOverweightException.java

---
+++ ShipOverweightException.java
@@ -0,0 +1,22 @@
+/**This is an Exception class that throws an error when a ship's capacity is exceeded
+ * 
+ * @author Pooja Ginjupalli
+ */
+
+public class ShipOverweightException extends Exception {
+	/**
+	 * This is a default Exception constructor that throws an error
+	 */
+	public ShipOverweightException() {
+		super();
+	}
+
+	/**
+	 * This is a Exception constructor that throws an error with a message
+	 * 
+	 * @param message The error message to be thrown with the error
+	 */
+	public ShipOverweightException(String message) {
+		super(message);
+	}
+}
